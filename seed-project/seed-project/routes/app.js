3b34fc7be5182918afa8f2eeaba13884958397bf
Volkers <renanvolkers@hotmail.com>
2017-10-14 23:44:55 -0300

    adição de view de erro

--- seed-project/seed-project/routes/app.js
+++ seed-project/seed-project/routes/app.js
@@ -5,4 +5,8 @@ router.get('/', function (req, res, next) {
     res.render('index');
 });
 
+router.get('/message', function (req, res, next) {
+    res.render('erro',{message:'Hello'});
+});
+
 module.exports = router;
