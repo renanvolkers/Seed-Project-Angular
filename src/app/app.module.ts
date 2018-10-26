55d79f1141a75458810924d6a3a25c92ab1f2d3d
Renan <renanvolkers@hotmail.com>
2018-10-26 00:18:08 -0300

    Atualização branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+
+
+import {ROUTES} from './app.routes'
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component'
+import { AboutComponent } from './about/about.component';
 import { RouterModule } from '@angular/router';
-
-
-import { AppComponent } from './app.component'
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+   HeaderComponent,
+    HomeComponent,
+    AboutComponent,
+    
   ],
   imports: [
     BrowserModule,
     HttpModule,
-    RouterModule
+    RouterModule.forRoot(ROUTES)
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
